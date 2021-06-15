f55065223e39506ee7823e55875355454499f8d0
eddierayo-156 <79113907+eddierayo-156@users.noreply.github.com>
2021-06-15 16:20:32 -0700

    Update game.js

--- game.js
+++ game.js
@@ -13,6 +13,8 @@ sprite.src = "img/sprite.png";
 const bg = new Image();
 bg.src = "img/bg.png";
 
+const bg2 = new Image();
+bg2.src = "img/csuf-2.png";
 
 // LOAD SOUNDS
 const SCORE_S = new Audio();
@@ -29,10 +31,6 @@ SWOOSHING.src = "audio/sfx_swooshing.wav";
 
 const DIE = new Audio();
 DIE.src = "audio/sfx_die.wav";
-
-let gamespeed = 1;
-var start = new Boolean(false);
-
 
 // GAME STATE
 const state = {
@@ -60,11 +58,9 @@ cvs.addEventListener("click", function(evt){
         case state.game:
             if(tuffy.y - tuffy.radius <= 0) return;
             tuffy.flap();
-            start = true;
             FLAP.play();
             break;
         case state.over:
-            start = false;
             let rect = cvs.getBoundingClientRect();
             let clickX = evt.clientX - rect.left;
             let clickY = evt.clientY - rect.top;
@@ -72,6 +68,7 @@ cvs.addEventListener("click", function(evt){
             if(clickX >= startBtn.x && clickX <= startBtn.x + startBtn.w && clickY >= startBtn.y && clickY <= startBtn.y + startBtn.h){
                 pipes.reset();
                 tuffy.speedReset();
+                score.reset();
                 state.current = state.getReady;
             }
             break;
@@ -84,35 +81,37 @@ const background = {
     y: -100,
     width: 1023,
     height: cvs.height,
-}
 
-function handleBackground() {
+    dx : 1,
 
-    if(start == true)
-    {
-        if(background.x1 <= -background.width + gamespeed) {
-            background.x1 = background.width;
-        }
-        else {
-            background.x1 -= gamespeed;
-        }
+    draw : function(){
+        ctx.drawImage(bg, this.x1, this.y, this.width, this.height);
+        ctx.drawImage(bg2, this.x2, this.y, this.width, this.height);
+    },
 
-        if(background.x2 <= -background.width + gamespeed){
-            background.x2 = background.width;
+    update: function(){
+        if(state.current == state.game){
+            if(background.x1 <= -background.width + this.dx) {
+                background.x1 = background.width;
+            }
+            else {
+                background.x1 -= this.dx;
+            }
+
+            if(background.x2 <= -background.width + this.dx){
+                background.x2 = background.width;
+            }
+            else (background.x2 -= this.dx);
         }
-        else (background.x2 -= gamespeed);
+        else
+        {
+            background.x1 = 0;
+            background.x2 = 1023;
+            background.y  = -100;
+            background.width = 1023;
+            background.height = 480;
+        }
     }
-    else
-    {
-        background.x1 = 0;
-        background.x2 = 1023;
-        background.y  = -100;
-        background.width = 1023;
-        background.height = 480;
-    }
-
-    ctx.drawImage(bg, background.x1, background.y, background.width, background.height);
-    ctx.drawImage(bg, background.x2, background.y, background.width, background.height);
 }
 
 const foreground = {
@@ -122,15 +121,14 @@ const foreground = {
     h: 112,
     x: 0,
     y: cvs.height - 112,
-    
+
     dx : 2,
-    
+
     draw : function(){
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
-        
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
     },
-    
+
     update: function(){
         if(state.current == state.game){
             this.x = (this.x - this.dx)%(this.w/2);
@@ -149,31 +147,31 @@ const tuffy = {
     y : 150,
     w : 34,
     h : 26,
-    
+
     radius : 12,
-    
+
     frame : 0,
-    
+
     gravity : 0.25,
     jump : 4.6,
     speed : 0,
     rotation : 0,
-    
+
     draw : function(){
         let bird = this.animation[this.frame];
-        
+
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(this.rotation);
         ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h,- this.w/2, - this.h/2, this.w, this.h);
-        
+
         ctx.restore();
     },
-    
+
     flap : function(){
         this.speed = - this.jump;
     },
-    
+
     update: function(){
         // IF THE GAME STATE IS GET READY STATE, THE BIRD MUST FLAP SLOWLY
         this.period = state.current == state.getReady ? 10 : 5;
@@ -181,14 +179,14 @@ const tuffy = {
         this.frame += frames%this.period == 0 ? 1 : 0;
         // FRAME GOES FROM 0 To 4, THEN AGAIN TO 0
         this.frame = this.frame%this.animation.length;
-        
+
         if(state.current == state.getReady){
             this.y = 150; // RESET POSITION OF THE BIRD AFTER GAME OVER
             this.rotation = 0 * DEGREE;
         }else{
             this.speed += this.gravity;
             this.y += this.speed;
-            
+
             if(this.y + this.h/2 >= cvs.height - foreground.h){
                 this.y = cvs.height - foreground.h - this.h/2;
                 if(state.current == state.game){
@@ -196,7 +194,7 @@ const tuffy = {
                     DIE.play();
                 }
             }
-            
+
             // IF THE SPEED IS GREATER THAN THE JUMP MEANS THE BIRD IS FALLING DOWN
             if(this.speed >= this.jump){
                 this.rotation = 90 * DEGREE;
@@ -205,7 +203,7 @@ const tuffy = {
                 this.rotation = -25 * DEGREE;
             }
         }
-        
+
     },
     speedReset : function(){
         this.speed = 0;
@@ -220,13 +218,13 @@ const getReady = {
     h : 152,
     x : cvs.width/2 - 173/2,
     y : 80,
-    
+
     draw: function(){
         if(state.current == state.getReady){
             ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
         }
     }
-    
+
 }
 
 // GAME OVER MESSAGE
@@ -237,19 +235,19 @@ const gameOver = {
     h : 202,
     x : cvs.width/2 - 225/2,
     y : 90,
-    
+
     draw: function(){
         if(state.current == state.over){
-            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);   
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
         }
     }
-    
+
 }
 
 // PIPES
 const pipes = {
     position : [],
-    
+
     top : {
         sX : 553,
         sY : 0
@@ -258,31 +256,31 @@ const pipes = {
         sX : 502,
         sY : 0
     },
-    
+
     w : 53,
     h : 400,
     gap : 150,
     maxYPos : -150,
     dx : 2,
-    
+
     draw : function(){
         for(let i  = 0; i < this.position.length; i++){
             let p = this.position[i];
-            
+
             let topYPos = p.y;
             let bottomYPos = p.y + this.h + this.gap;
-            
+
             // top pipe
-            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);  
-            
+            ctx.drawImage(sprite, this.top.sX, this.top.sY, this.w, this.h, p.x, topYPos, this.w, this.h);
+
             // bottom pipe
-            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);  
+            ctx.drawImage(sprite, this.bottom.sX, this.bottom.sY, this.w, this.h, p.x, bottomYPos, this.w, this.h);
         }
     },
-    
+
     update: function(){
         if(state.current !== state.game) return;
-        
+
         if(frames%150 == 0){
             this.position.push({
                 x : cvs.width,
@@ -291,9 +289,9 @@ const pipes = {
         }
         for(let i = 0; i < this.position.length; i++){
             let p = this.position[i];
-            
+
             let bottomPipeYPos = p.y + this.h + this.gap;
-            
+
             // COLLISION DETECTION
             // TOP PIPE
             if(tuffy.x + tuffy.radius > p.x && tuffy.x - tuffy.radius < p.x + this.w && tuffy.y + tuffy.radius > p.y && tuffy.y - tuffy.radius < p.y + this.h){
@@ -305,36 +303,73 @@ const pipes = {
                 state.current = state.over;
                 HIT.play();
             }
-            
+
             // MOVE THE PIPES TO THE LEFT
             p.x -= this.dx;
-            
+
             // if the pipes go beyond canvas, we delete them from the array
             if(p.x + this.w <= 0){
                 this.position.shift();
+                score.value += 1;
+                SCORE_S.play();
+                score.best = Math.max(score.value, score.best);
+                localStorage.setItem("best", score.best);
             }
         }
     },
-    
+
     reset : function(){
         this.position = [];
     }
-    
+
+}
+
+// SCORE
+const score= {
+    best : parseInt(localStorage.getItem("best")) || 0,
+    value : 0,
+
+    draw : function(){
+        ctx.fillStyle = "#FFF";
+        ctx.strokeStyle = "#000";
+
+        if(state.current == state.game){
+            ctx.lineWidth = 2;
+            ctx.font = "35px Teko";
+            ctx.fillText(this.value, cvs.width/2, 50);
+            ctx.strokeText(this.value, cvs.width/2, 50);
+
+        }else if(state.current == state.over){
+            // SCORE VALUE
+            ctx.font = "25px Teko";
+            ctx.fillText(this.value, 225, 186);
+            ctx.strokeText(this.value, 225, 186);
+            // BEST SCORE
+            ctx.fillText(this.best, 225, 228);
+            ctx.strokeText(this.best, 225, 228);
+        }
+    },
+
+    reset : function(){
+        this.value = 0;
+    }
 }
 
 function draw(){
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
 
-    handleBackground();
+    background.draw();
     pipes.draw();
     foreground.draw();
     tuffy.draw();
     getReady.draw();
     gameOver.draw();
+    score.draw();
 }
 
 function update(){
+    background.update();
     tuffy.update();
     foreground.update();
     pipes.update();
